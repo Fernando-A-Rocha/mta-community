959dc23266d3c89c416e33acef7933e223f5b72d
Fernando-A-Rocha <frocha2001@gmail.com>
2025-11-29 00:38:58 +0000

    c

--- comm2/app/Data/ProfileFavorites.php
+++ comm2/app/Data/ProfileFavorites.php
@@ -28,11 +28,25 @@ class ProfileFavorites
     public static function vehicles(): array
     {
         return [
-            'Vehicle 1',
-            'Vehicle 2',
-            'Vehicle 3',
-            'Vehicle 4',
-            'Vehicle 5',
+            "Landstalker", "Bravura", "Buffalo", "Linerunner", "Perennial", "Sentinel", "Dumper", "Fire Truck", "Trashmaster", "Stretch", "Manana",
+  "Infernus", "Voodoo", "Pony", "Mule", "Cheetah", "Ambulance", "Leviathan", "Moonbeam", "Esperanto", "Taxi", "Washington", "Bobcat",
+  "Mr. Whoopee", "BF Injection", "Hunter", "Premier", "Enforcer", "Securicar", "Banshee", "Predator", "Bus", "Rhino", "Barracks", "Hotknife",
+  "Trailer 1", "Previon", "Coach", "Cabbie", "Stallion", "Rumpo", "RC Bandit", "Romero", "Packer", "Monster", "Admiral", "Squalo",
+  "Seasparrow", "Pizzaboy", "Tram", "Trailer 2", "Turismo", "Speeder", "Reefer", "Tropic", "Flatbed", "Yankee", "Caddy", "Solair",
+  "Berkley's RC Van", "Skimmer", "PCJ-600", "Faggio", "Freeway", "RC Baron", "RC Raider", "Glendale", "Oceanic", "Sanchez", "Sparrow", "Patriot",
+  "Quadbike", "Coastguard", "Dinghy", "Hermes", "Sabre", "Rustler", "ZR-350", "Walton", "Regina", "Comet", "BMX", "Burrito", "Camper", "Marquis",
+  "Baggage", "Dozer", "Maverick", "News Chopper", "Rancher", "FBI Rancher", "Virgo", "Greenwood", "Jetmax", "Hotring Racer", "Sandking",
+  "Blista Compact", "Police Maverick", "Boxville", "Benson", "Mesa", "RC Goblin", "Hotring Racer 2", "Hotring Racer 3", "Bloodring Banger",
+  "Rancher Lure", "Super GT", "Elegant", "Journey", "Bike", "Mountain Bike", "Beagle", "Cropduster", "Stuntplane", "Tanker", "Roadtrain", "Nebula",
+  "Majestic", "Buccaneer", "Shamal", "Hydra", "FCR-900", "NRG-500", "HPV1000", "Cement Truck", "Towtruck", "Fortune", "Cadrona", "FBI Truck",
+  "Willard", "Forklift", "Tractor", "Combine Harvester", "Feltzer", "Remington", "Slamvan", "Blade", "Freight", "Brown Streak", "Vortex", "Vincent",
+  "Bullet", "Clover", "Sadler", "Fire Truck Ladder", "Hustler", "Intruder", "Primo", "Cargobob", "Tampa", "Sunrise", "Merit", "Utility Van",
+  "Nevada", "Yosemite", "Windsor", "Monster 2", "Monster 3", "Uranus", "Jester", "Sultan", "Stratum", "Elegy", "Raindance", "RC Tiger", "Flash",
+  "Tahoma", "Savanna", "Bandito", "Freight Train Flatbed", "Streak Train Trailer", "Kart", "Mower", "Dune", "Sweeper", "Broadway", "Tornado",
+  "AT-400", "DFT-30", "Huntley", "Stafford", "BF-400", "Newsvan", "Tug", "Trailer (Tanker Commando)", "Emperor", "Wayfarer", "Euros", "Hotdog",
+  "Club", "Box Freight", "Trailer 3", "Andromada", "Dodo", "RC Cam", "Launch", "Police LS", "Police SF", "Police LV", "Police Ranger",
+  "Picador", "S.W.A.T.", "Alpha", "Phoenix", "Glendale Damaged", "Sadler Damaged", "Baggage Trailer (covered)",
+  "Baggage Trailer (Uncovered)", "Trailer (Stairs)", "Boxville Mission", "Farm Trailer", "Street Clean Trailer"
         ];
     }
 
@@ -44,11 +58,8 @@ class ProfileFavorites
     public static function characters(): array
     {
         return [
-            'Character 1',
-            'Character 2',
-            'Character 3',
-            'Character 4',
-            'Character 5',
+            'Carl "CJ" Johnson',
+        'Sweet Johnson', 'Kendl Johnson', 'Big Smoke', 'Ryder', 'Cesar Vialpando', 'Catalina', 'Claude', 'The Truth', 'Wu Zi Mu', 'Zero', 'Jizzy B.', 'T-Bone Mendez', 'Mike Toreno', 'Madd Dogg', 'OG Loc', 'Kent Paul', 'Maccer', 'Ken Rosenberg', 'Officer Frank Tenpenny', 'Officer Eddie Pulaski', 'Officer Jimmy Hernandez', 'Emmet'
         ];
     }
 
@@ -60,11 +71,17 @@ class ProfileFavorites
     public static function gangs(): array
     {
         return [
-            'Gang 1',
-            'Gang 2',
-            'Gang 3',
-            'Gang 4',
-            'Gang 5',
+            'Grove Street Families',
+            'Ballas',
+            'Los Santos Vagos',
+            'Varios Los Aztecas',
+            'San Fierro Rifa',
+            'Da Nang Boys',
+            'Triads',
+            'Italian Mafia',
+            'Russian Mafia',
+            'Varrio Los Aztecas',
+            'The Loco Syndicate'
         ];
     }
 
@@ -76,11 +93,50 @@ class ProfileFavorites
     public static function weapons(): array
     {
         return [
-            'Weapon 1',
-            'Weapon 2',
-            'Weapon 3',
-            'Weapon 4',
-            'Weapon 5',
+            'Fist',
+            'Brassknuckle',
+            'Golfclub',
+            'Nightstick',
+            'Knife',
+            'Bat',
+            'Shovel',
+            'Poolstick',
+            'Katana',
+            'Chainsaw',
+            'Colt 45',
+            'Silenced',
+            'Deagle',
+            'Shotgun',
+            'Sawed-off',
+            'Combat Shotgun',~
+            'Uzi',
+            'MP5',
+            'Tec-9',
+            'AK-47',
+            'M4',
+            'Rifle',
+            'Sniper',
+            'Rocket Launcher',
+            'Rocket Launcher HS',
+            'Flamethrower',
+            'Minigun',
+            'Grenade',
+            'Teargas',
+            'Molotov',
+            'Satchel',
+            'Spraycan',
+            'Fire Extinguisher',
+            'Camera',
+            'Dildo',
+            'Purple Dildo',
+            'Vibrator',
+            'Silver Vibrator',
+            'Flower',
+            'Cane',
+            'Nightvision Goggles',
+            'Infrared Goggles',
+            'Parachute',
+            'Satchel Detonator'
         ];
     }
 
@@ -92,11 +148,19 @@ class ProfileFavorites
     public static function radioStations(): array
     {
         return [
-            'Radio Station 1',
-            'Radio Station 2',
-            'Radio Station 3',
-            'Radio Station 4',
-            'Radio Station 5',
+            'Radio Los Santos',
+            'Playback FM',
+            'K-DST',
+            'Bounce FM',
+            'KJAH West',
+            'Master Sounds 98.3',
+            'CSR 103.9',
+            'Radio X',
+            'SF-UR',
+            'West Coast Talk Radio',
+            'K-Rose',
+            'K-Jah Radio West',
+            'User Track Player'
         ];
     }
 }
