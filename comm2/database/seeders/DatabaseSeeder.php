9247c72784072775447b2f798b90de442061059c
Fernando-A-Rocha <frocha2001@gmail.com>
2025-11-29 10:11:23 +0000

    Update user seeder

--- comm2/database/seeders/DatabaseSeeder.php
+++ comm2/database/seeders/DatabaseSeeder.php
@@ -11,10 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-        ]);
-
+        // Mandatory:
         $this->call([
             TagSeeder::class,
         ]);
--- comm2/database/seeders/UserSeeder.php
+++ comm2/database/seeders/UserSeeder.php
@@ -8,19 +8,18 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
+/**
+ * OPTIONAL SEEDER FOR TESTING
+ */
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $this->createUser('test1', 'test1@example.com', 'password');
-        $this->createUser('test2', 'test2@example.com', 'password');
-        // Admin
-        $this->createUser('admin', 'admin@example.com', 'password', 'admin');
-        // Moderator
-        $this->createUser('moderator', 'moderator@example.com', 'password', 'moderator');
+        $password = "!Super25u8StrongPW$";
+        $this->createUser('test1', 'test1@example.com', $password);
+        $this->createUser('test2', 'test2@example.com', $password);
+        $this->createUser('admin', 'admin@example.com', $password, 'admin');
+        $this->createUser('moderator', 'moderator@example.com', $password, 'moderator');
     }
 
     private function createUser(string $name, string $email, string $password, string $role = 'user'): void
